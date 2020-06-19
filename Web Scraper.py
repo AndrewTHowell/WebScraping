2329cc7cffc83554ceaaf61ca5c1b4aea3e02949
AndrewTHowell <howelldrew99@gmail.com>
2020-06-19 16:01:20 +0100

    Basic web scraping calls

--- Web Scraper.py
+++ Web Scraper.py
@@ -1,3 +1,13 @@
 import requests
 import lxml
 import bs4
+
+source = requests.get("https://devopscube.com/project-management-software").text
+
+soup = bs4.BeautifulSoup(source, "lxml")
+
+article = soup.find("article")
+
+headline = article.div.h3.text
+
+print(headline)
